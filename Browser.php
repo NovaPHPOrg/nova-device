dd4033c679974eca6d0c8e31d600df03d08c9cf5
ankio <ankio@ankio.net>
2025-07-19 10:31:10 +0800

    :sparkles: (core): 完善框架核心组件文档和注释
    :bulb: 为Config、StaticRegister、NovaApp等核心类添加详细的文档注释
    :memo: 更新README，全面介绍Nova框架特性、目录结构和核心组件
    :recycle: 重构部分代码结构，优化语意和可读性
    :label: 为方法参数和返回值添加类型声明，提升代码安全性
    :children_crossing: 完善使用示例，提升开发者体验

--- Browser.php
+++ Browser.php
@@ -10,59 +10,102 @@ declare(strict_types=1);
  * Created By ankio.
  * Date : 2023/7/13
  * Time : 21:06
- * Description :
+ * Description : 浏览器识别类，用于解析 User-Agent 字符串并识别浏览器类型和版本
  */
 
 namespace nova\plugin\device;
 
+/**
+ * 浏览器识别类
+ *
+ * 该类用于解析 HTTP User-Agent 字符串，识别用户使用的浏览器类型、版本等信息。
+ * 支持识别多种主流浏览器，包括桌面浏览器和移动浏览器。
+ *
+ * @author ankio
+ * @since 2023/7/13
+ */
 class Browser
 {
+    /**
+     * 解析 User-Agent 字符串，识别浏览器信息
+     *
+     * 该方法通过正则表达式匹配 User-Agent 字符串中的特定标识符，
+     * 识别用户使用的浏览器类型、版本等信息。
+     *
+     * @param  string $ua User-Agent 字符串
+     * @return array  返回包含浏览器信息的数组
+     *                - code: 浏览器代码标识符
+     *                - title: 浏览器完整名称（包含版本号）
+     */
     public static function get($ua)
     {
-        $version = '';
-        $code = null;
+        $version = '';        // 浏览器版本号
+        $code = null;         // 浏览器代码标识符
 
+        // 360 安全浏览器识别
         if (preg_match('/360se/i', $ua)) {
             $title = '360 安全浏览器';
             $code = '360';
-        } elseif (preg_match('/baidubrowser/i', $ua) || preg_match('/\ Spark/i', $ua)) {
+        }
+        // 百度浏览器识别
+        elseif (preg_match('/baidubrowser/i', $ua) || preg_match('/\ Spark/i', $ua)) {
             $title = '百度浏览器';
             $version = Version::get($ua, 'Browser');
             $code = 'BaiduBrowser';
-        } elseif (preg_match('/SE\ /i', $ua) && preg_match('/MetaSr/i', $ua)) {
+        }
+        // 搜狗高速浏览器识别
+        elseif (preg_match('/SE\ /i', $ua) && preg_match('/MetaSr/i', $ua)) {
             $title = '搜狗高速浏览器';
             $code = 'Sogou-Explorer';
-        } elseif (preg_match('/QQBrowser/i', $ua) || preg_match('/MQQBrowser/i', $ua)) {
+        }
+        // QQ 浏览器识别
+        elseif (preg_match('/QQBrowser/i', $ua) || preg_match('/MQQBrowser/i', $ua)) {
             $title = 'QQ 浏览器';
             $version = Version::get($ua, 'QQBrowser');
             $code = 'QQBrowser';
-        } elseif (preg_match('/chromeframe/i', $ua)) {
+        }
+        // Google Chrome Frame 识别
+        elseif (preg_match('/chromeframe/i', $ua)) {
             $title = 'Google Chrome Frame';
             $version = Version::get($ua, 'chromeframe');
             $code = 'Chrome';
-        } elseif (preg_match('/Chromium/i', $ua)) {
+        }
+        // Chromium 浏览器识别
+        elseif (preg_match('/Chromium/i', $ua)) {
             $title = 'Chromium';
             $version = Version::get($ua, 'Chromium');
-        } elseif (preg_match('/CrMo/i', $ua)) {
+        }
+        // Google Chrome Mobile 识别
+        elseif (preg_match('/CrMo/i', $ua)) {
             $title = 'Google Chrome Mobile';
             $version = Version::get($ua, 'CrMo');
             $code = 'Chrome';
-        } elseif (preg_match('/CriOS/i', $ua)) {
+        }
+        // Google Chrome for iOS 识别
+        elseif (preg_match('/CriOS/i', $ua)) {
             $title = 'Google Chrome for iOS';
             $version = Version::get($ua, 'CriOS');
             $code = 'Chrome';
-        } elseif (preg_match('/Maxthon/i', $ua)) {
+        }
+        // 傲游浏览器识别
+        elseif (preg_match('/Maxthon/i', $ua)) {
             $title = '傲游浏览器';
             $version = Version::get($ua, 'Maxthon');
             $code = 'Maxthon';
-        } elseif (preg_match('/MiuiBrowser/i', $ua)) {
+        }
+        // MIUI Browser 识别
+        elseif (preg_match('/MiuiBrowser/i', $ua)) {
             $title = 'MIUI Browser';
             $version = Version::get($ua, 'MiuiBrowser');
             $code = 'MIUI-Browser';
-        } elseif (preg_match('/TheWorld/i', $ua)) {
+        }
+        // 世界之窗浏览器识别
+        elseif (preg_match('/TheWorld/i', $ua)) {
             $title = '世界之窗浏览器';
             $code = 'TheWorld';
-        } elseif (preg_match('/UBrowser/i', $ua)) {
+        }
+        // UC 浏览器识别（多种标识符）
+        elseif (preg_match('/UBrowser/i', $ua)) {
             $title = 'UC 浏览器';
             $version = Version::get($ua, 'UBrowser');
             $code = 'UC';
@@ -78,39 +121,61 @@ class Browser
             $title = 'UC 浏览器';
             $version = Version::get($ua, 'UCWEB');
             $code = 'UC';
-        } elseif (preg_match('/BlackBerry/i', $ua)) {
+        }
+        // BlackBerry 浏览器识别
+        elseif (preg_match('/BlackBerry/i', $ua)) {
             $title = 'BlackBerry';
-        } elseif (preg_match('/Coast/i', $ua)) {
+        }
+        // Coast 浏览器识别
+        elseif (preg_match('/Coast/i', $ua)) {
             $title = 'Coast';
             $version = Version::get($ua, 'Coast');
             $code = 'Opera';
-        } elseif (preg_match('/IEMobile/i', $ua)) {
+        }
+        // IE Mobile 识别
+        elseif (preg_match('/IEMobile/i', $ua)) {
             $title = 'IE Mobile';
             $code = 'IE';
-        } elseif (preg_match('/LG Browser/i', $ua)) {
+        }
+        // LG Web Browser 识别
+        elseif (preg_match('/LG Browser/i', $ua)) {
             $title = 'LG Web Browser';
             $version = Version::get($ua, 'Browser');
             $code = 'LG';
-        } elseif (preg_match('/Navigator/i', $ua)) {
+        }
+        // Netscape Navigator 识别
+        elseif (preg_match('/Navigator/i', $ua)) {
             $title = 'Netscape Navigator';
             $code = 'Netscape';
-        } elseif (preg_match('/Netscape/i', $ua)) {
+        }
+        // Netscape 浏览器识别
+        elseif (preg_match('/Netscape/i', $ua)) {
             $title = 'Netscape';
-        } elseif (preg_match('/Nintendo 3DS/i', $ua)) {
+        }
+        // Nintendo 3DS 浏览器识别
+        elseif (preg_match('/Nintendo 3DS/i', $ua)) {
             $title = 'Nintendo 3DS';
             $code = 'Nintendo';
-        } elseif (preg_match('/NintendoBrowser/i', $ua)) {
+        }
+        // Nintendo Browser 识别
+        elseif (preg_match('/NintendoBrowser/i', $ua)) {
             $title = 'Nintendo Browser';
             $version = Version::get($ua, 'Browser');
             $code = 'Nintendo';
-        } elseif (preg_match('/NokiaBrowser/i', $ua)) {
+        }
+        // Nokia Browser 识别
+        elseif (preg_match('/NokiaBrowser/i', $ua)) {
             $title = 'Nokia Browser';
             $version = Version::get($ua, 'Browser');
             $code = 'Nokia';
-        } elseif (preg_match('/Opera Mini/i', $ua)) {
+        }
+        // Opera Mini 识别
+        elseif (preg_match('/Opera Mini/i', $ua)) {
             $title = 'Opera Mini';
             $code = 'Opera';
-        } elseif (preg_match('/Opera Mobi/i', $ua)) {
+        }
+        // Opera Mobile 识别
+        elseif (preg_match('/Opera Mobi/i', $ua)) {
             if (preg_match('/Version/i', $ua)) {
                 $version = Version::get($ua, 'Version');
             } else {
@@ -118,97 +183,131 @@ class Browser
             }
             $title = 'Opera Mobile';
             $code = 'Opera';
-        } elseif (preg_match('/Opera/i', $ua) || preg_match('/OPR/i', $ua)) {
+        }
+        // Opera 浏览器识别（桌面版和移动版）
+        elseif (preg_match('/Opera/i', $ua) || preg_match('/OPR/i', $ua)) {
             $title = 'Opera';
             $code = 'Opera';
-            // How is version stored on this Opera ua?
+            // 获取 Opera 版本号（支持多种版本标识符）
             if (preg_match('/Version/i', $ua)) {
                 $version = Version::get($ua, 'Version');
             } elseif (preg_match('/OPR/i', $ua)) {
                 $version = Version::get($ua, 'OPR');
             } else {
-                // Use Opera as fallback since full title may change (Next, Developer, etc.)
+                // 使用 Opera 作为后备标识符，因为完整标题可能会变化（Next, Developer 等）
                 $version = Version::get($ua, 'Opera');
             }
-            // Parse full edition name, ex: Opera/9.80 (X11; Linux x86_64; U; Edition Labs Camera and Pages; Ubuntu/11.10; en) Presto/2.9.220 Version/12.00
+            // 解析完整版本名称，例如：Opera/9.80 (X11; Linux x86_64; U; Edition Labs Camera and Pages; Ubuntu/11.10; en) Presto/2.9.220 Version/12.00
             if (preg_match('/Edition ([\ ._0-9a-zA-Z]+)/i', $ua, $regmatch)) {
                 $title .= ' ' . $regmatch[1];
             } elseif (preg_match('/Opera ([\ ._0-9a-zA-Z]+)/i', $ua, $regmatch)) {
                 $title .= ' ' . $regmatch[1];
             }
-        } elseif (preg_match('/PlayStation\ 4/i', $ua)) {
+        }
+        // PS4 Web Browser 识别
+        elseif (preg_match('/PlayStation\ 4/i', $ua)) {
             $title = 'PS4 Web Browser';
             $code = 'PS4';
-        } elseif (preg_match('/SEMC-Browser/i', $ua)) {
+        }
+        // SEMC Browser 识别
+        elseif (preg_match('/SEMC-Browser/i', $ua)) {
             $title = 'SEMC Browser';
             $version = Version::get($ua, 'SEMC-Browser');
             $code = 'Sony';
-        } elseif (preg_match('/SEMC-java/i', $ua)) {
+        }
+        // SEMC Java 识别
+        elseif (preg_match('/SEMC-java/i', $ua)) {
             $title = 'SEMC Java';
             $code = 'Sony';
-        } elseif (preg_match('/Series60/i', $ua) && !preg_match('/Symbian/i', $ua)) {
+        }
+        // Nokia S60 识别（Series60 标识符）
+        elseif (preg_match('/Series60/i', $ua) && !preg_match('/Symbian/i', $ua)) {
             $title = 'Nokia S60';
             $version = Version::get($ua, 'Series60');
             $code = 'Nokia';
-        } elseif (preg_match('/S60/i', $ua) && !preg_match('/Symbian/i', $ua)) {
+        }
+        // Nokia S60 识别（S60 标识符）
+        elseif (preg_match('/S60/i', $ua) && !preg_match('/Symbian/i', $ua)) {
             $title = 'Nokia S60';
             $version = Version::get($ua, 'S60');
             $code = 'Nokia';
-        } elseif (preg_match('/TencentTraveler/i', $ua)) {
+        }
+        // TT 浏览器识别
+        elseif (preg_match('/TencentTraveler/i', $ua)) {
             $title = 'TT 浏览器';
             $version = Version::get($ua, 'TencentTraveler');
             $code = 'QQBrowser';
-        } elseif ((preg_match('/Ubuntu\;\ Mobile/i', $ua) || preg_match('/Ubuntu\;\ Tablet/i', $ua) && preg_match('/WebKit/i', $ua))) {
+        }
+        // Ubuntu Web Browser 识别
+        elseif ((preg_match('/Ubuntu\;\ Mobile/i', $ua) || preg_match('/Ubuntu\;\ Tablet/i', $ua) && preg_match('/WebKit/i', $ua))) {
             $title = 'Ubuntu Web Browser';
             $code = 'Ubuntu';
-        } elseif (preg_match('/AppleWebkit/i', $ua) && preg_match('/Android/i', $ua) && !preg_match('/Chrome/i', $ua)) {
+        }
+        // Android Webkit 识别
+        elseif (preg_match('/AppleWebkit/i', $ua) && preg_match('/Android/i', $ua) && !preg_match('/Chrome/i', $ua)) {
             $title = 'Android Webkit';
             $version = Version::get($ua, 'Version');
             $code = 'Android-Webkit';
-        } elseif (preg_match('/Chrome/i', $ua) && preg_match('/Mobile/i', $ua) && (preg_match('/Version/i', $ua) || preg_match('/wv/i', $ua))) {
+        }
+        // WebView 识别
+        elseif (preg_match('/Chrome/i', $ua) && preg_match('/Mobile/i', $ua) && (preg_match('/Version/i', $ua) || preg_match('/wv/i', $ua))) {
             $title = 'WebView';
             $version = Version::get($ua, 'Version');
             $code = 'Android-Webkit';
         }
-        // Pulled out of order to help ensure better detection for above browsers
+        // 以下浏览器识别顺序调整，以确保更好的检测效果
+        // Microsoft Edge 识别（需要同时匹配 Edge、Chrome 和 Safari）
         elseif (preg_match('/Edge/i', $ua) && preg_match('/Chrome/i', $ua) && preg_match('/Safari/i', $ua)) {
             $title = 'Microsoft Edge';
             $version = Version::get($ua, 'Edge');
             $code = 'Edge';
-        } elseif (preg_match('/Chrome/i', $ua)) {
+        }
+        // Google Chrome 识别
+        elseif (preg_match('/Chrome/i', $ua)) {
             $title = 'Google Chrome';
             $version = Version::get($ua, 'Chrome');
             $code = 'Chrome';
-        } elseif (preg_match('/Safari/i', $ua) && !preg_match('/Nokia/i', $ua)) {
+        }
+        // Safari 浏览器识别
+        elseif (preg_match('/Safari/i', $ua) && !preg_match('/Nokia/i', $ua)) {
             $title = 'Safari';
             $code = 'Safari';
             if (preg_match('/Version/i', $ua)) {
                 $version = Version::get($ua, 'Version');
             }
+            // 检测移动版 Safari
             if (preg_match('/Mobile Safari/i', $ua)) {
                 $title = 'Mobile ' . $title;
             }
-        } elseif (preg_match('/Nokia/i', $ua)) {
+        }
+        // Nokia Web Browser 识别
+        elseif (preg_match('/Nokia/i', $ua)) {
             $title = 'Nokia Web Browser';
             $code = 'Nokia';
-        } elseif (preg_match('/Firefox/i', $ua)) {
+        }
+        // Firefox 浏览器识别
+        elseif (preg_match('/Firefox/i', $ua)) {
             $title = 'Firefox';
             $version = Version::get($ua, 'Firefox');
-        } elseif (preg_match('/MSIE/i', $ua) || preg_match('/Trident/i', $ua)) {
+        }
+        // Internet Explorer 识别（包括 Trident 引擎）
+        elseif (preg_match('/MSIE/i', $ua) || preg_match('/Trident/i', $ua)) {
             $title = 'Internet Explorer';
             $code = 'IE';
             if (preg_match('/\ rv:([.0-9a-zA-Z]+)/i', $ua)) {
-                // IE11 or newer
+                // IE11 或更新版本
                 $version = Version::get($ua, ' rv');
             } else {
-                // IE10 or older, regex: '/MSIE[\ |\/]?([.0-9a-zA-Z]+)/i'
+                // IE10 或更早版本，正则表达式：'/MSIE[\ |\/]?([.0-9a-zA-Z]+)/i'
                 $version = Version::get($ua, 'MSIE');
             }
-            // Detect compatibility mode for IE
+            // 检测 IE 兼容模式
             if ($version === '7.0' && preg_match('/Trident\/4.0/i', $ua)) {
                 $version = '8.0 (Compatibility Mode)';
             }
-        } elseif (preg_match('/Mozilla/i', $ua)) {
+        }
+        // Mozilla 兼容浏览器识别
+        elseif (preg_match('/Mozilla/i', $ua)) {
             $title = 'Mozilla';
             $version = Version::get($ua, ' rv');
             if (empty($version)) {
@@ -216,18 +315,23 @@ class Browser
                 $code = 'Mozilla';
             }
         }
-        // No Web browser match
+        // 未匹配到任何已知浏览器
         else {
             $title = 'Other Browser';
             $code = 'Others';
         }
+
+        // 如果没有设置代码标识符，使用标题作为代码
         if (is_null($code)) {
             $code = $title;
         }
+
+        // 如果有版本号，将其添加到标题中
         if ($version != '') {
             $title .= " $version";
         }
 
+        // 构建返回结果数组
         $result['code'] = $code;
         $result['title'] = $title;
         return $result;
--- Version.php
+++ Version.php
@@ -10,16 +10,35 @@ declare(strict_types=1);
  * Created By ankio.
  * Date : 2023/7/13
  * Time : 21:06
- * Description :
+ * Description : 版本号提取工具类，用于从 User-Agent 字符串中提取浏览器版本号
  */
 
 namespace nova\plugin\device;
 
+/**
+ * 版本号提取工具类
+ *
+ * 该类用于从 User-Agent 字符串中提取浏览器版本号信息。
+ * 通过正则表达式匹配特定的版本标识符来获取版本号。
+ *
+ * @author ankio
+ * @since 2023/7/13
+ */
 class Version
 {
+    /**
+     * 从 User-Agent 字符串中提取指定标识符的版本号
+     *
+     * 该方法使用正则表达式在 User-Agent 字符串中查找指定的标识符，
+     * 并提取其后面的版本号信息。
+     *
+     * @param  string $ua    User-Agent 字符串
+     * @param  string $title 要查找的版本标识符（如 'Chrome', 'Firefox', 'Version' 等）
+     * @return string 返回提取到的版本号，如果未找到则返回空字符串
+     */
     public static function get($ua, $title)
     {
-        // Grab the browser version if it's present
+        // 使用正则表达式匹配版本号，支持空格、斜杠或冒号作为分隔符
         preg_match('/' . $title . '[\ |\/|\:]?([.0-9a-zA-Z]+)/i', $ua, $regmatch);
         return (!isset($regmatch[1])) ? '' : $regmatch[1];
     }
